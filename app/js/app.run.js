c09e92f1fedf4c67104aa17a4e849f74216319c2
Abderrazzek Ben Hammouda <abderrazzek.benhammodua@esprit.tn>
2019-12-07 22:16:25 +0100

    Final

--- app/js/app.run.js
+++ app/js/app.run.js
@@ -3,4 +3,7 @@
 angular.module('LpcWebTest2018')
     .run(['LpcTranslateService','RESOURCES',function(LpcTranslateService,RESOURCES){
         //TODO
+        //Consume LpcTranslateService service
+        LpcTranslateService.setPropertiesUrl(RESOURCES.PROPERTIES_URL);
+        LpcTranslateService.loadProperties();
     }]);
--- app/js/lpc-translate.filter.js
+++ app/js/lpc-translate.filter.js
@@ -4,8 +4,14 @@ angular.module('LpcWebTest2018')
     .filter('lpcTranslate',['LpcTranslateService','$rootScope',function(LpcTranslateService,$rootScope){
         //TODO
         var filter = function(key,locale){
-            //TODO
+            //If data not loaded fill with placeholder empty string
+            if(!$rootScope.properties) return " " ;
+            else return $rootScope.properties[locale][key];
         }
-        //TODO
+
+        //Filter needs to be stateful to run each digest cycle
+        filter.$stateful=true;
+
+
         return filter;
     }]);
